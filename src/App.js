caba27c6b0fea26dd0e3130fcf4cdb003d8bb9b9
nwooll <neelamwoolley@gmail.com>
2018-02-26 22:14:13 -0800

    master
    stciker

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import layer1 from './imgs/layer1.png';
 import layer2 from './imgs/layer2.png';
 import './App.css';
 import mySocket from 'socket.io-client';
+import Rooms from './comp/Rooms';
 
 class App extends Component {
     constructor(props){
@@ -16,24 +17,43 @@ class App extends Component {
             myMsg:"",
             bgPos: false,
             chatDisp: false,
-            chatBar: false
+            chatBar: false,
+			nameCont : false,
+			sBtnCont: false,
+			sPage: false,
+			
+			myImg:require("./imgs/hair.gif"),
+			myImg2:require("./imgs/1.gif"),
+			allUsers:[],
+			myId: null,
+			showDisplay:false,
+			stickers:[]
         }
         this.joinChat = this.joinChat.bind(this);
         this.handleUsername = this.handleUsername.bind(this);
         this.handleMyMsg = this.handleMyMsg.bind(this);
         this.sendChat = this.sendChat.bind(this);
+		this.showStickerPage = this.showStickerPage.bind(this);
+		
+		this.handleImage = this.handleImage.bind(this);
+		this.changePage = this.changePage.bind(this);
     }
     
     componentDidMount(){
-        //this.socket =mySocket("http://localhost:10001");
-    }
+		
+	}
     
+	
+	
     joinChat(){
         this.setState({
             mode:1,
+			sPage: false,
             bgPos: true,
             chatDisp: true,
-            chatBar: true
+            chatBar: true,
+			nameCont: true,
+			sBtnCont: true
         })
         this.socket = mySocket("https://dynacontsocket.herokuapp.com/");
         this.socket.emit("username", this.state.username);
@@ -70,10 +90,94 @@ class App extends Component {
         this.socket.emit("sendChat", msg);
         
     }
+	
+	showStickerPage(){
+		this.setState({
+			sPage: true,
+			 bgPos: false,
+            chatDisp: false,
+            chatBar: false,
+			nameCont: false,
+			sBtnCont: false,
+			mode: 0
+			
+		})
+		
+		this.socket = mySocket("http://localhost:10001");
+		
+		this.socket.on("createimage", (data)=>{
+			this.setState({
+				allUsers:data
+			});
+			
+			this.refs.thedisplay.addEventListener("mousemove", (ev)=>{
+		
+			if(this.state.myId == null){
+				return false;
+			}
+			this.refs["u"+this.state.myId].style.left = ev.pageX+"px";
+			this.refs["u"+this.state.myId].style.top = ev.pageY+"px";
+			
+			this.socket.emit("mymove", {
+				x:ev.pageX,
+				y:ev.pageY,
+				id:this.state.myId,
+				img:this.refs["u"+this.state.myId].src
+			});
+		});
+			this.refs.thedisplay.addEventListener("click", (ev)=>{
+				this.socket.emit("sticker",{
+					x:ev.pageX,
+					y:ev.pageY,
+					id:this.state.myId,
+					src:this.refs["u"+this.state.myId].src
+				})
+			})
+		});
+		
+		
+		this.socket.on("newsticker", (data)=>{
+		this.setState({
+			stickers:data
+		})	
+		});
+		
+		
+		this.socket.on("yourid", (data)=>{
+			this.setState({
+				myId:data
+			})
+		});
+		
+		
+		this.socket.on("usermove", (data)=>{
+			console.log("user has moved");
+			this.refs["u"+data.id].style.top = data.y+"px";
+			this.refs["u"+data.id].style.left = data.x+"px";
+			this.refs["u"+data.id].src = data.img;
+		});
+		
+	}
+	
+
+	
+	
+	handleImage(evt){
+		this.refs["u"+this.state.myId].src = evt.target.src;
+	}
+	
+	changePage(roomString){
+			this.setState({
+				showDisplay:true
+			});
+		
+		this.socket.emit("joinroom", roomString);
+	}
+	
     
   render() {
       
-       var chatBarShow = "";
+      var chatBarShow = "";
       
       if(this.state.chatBar === true){
           chatBarShow = " chatBarShow";
@@ -84,7 +188,25 @@ class App extends Component {
       if(this.state.chatDisp === true){
           chatBoxShow = " chatBoxShow";
       }
+	  
+	  var nameContShow = "";
+	  
+	  if(this.state.nameCont === true){
+		  nameContShow = " nameContShow";
+	  }
+	  
+	  var stickerRoomsShow = "";
+	  
+	  if(this.state.sBtnCont === true){
+		  stickerRoomsShow = " stickerRoomsShow";
+	  }
       
+	  var stickerPageShow = "";
+	  
+	  if(this.state.sPage === true){
+		  stickerPageShow = " stickerPageShow";
+	  }
+	  
       var bgChange = "";
       
       if(this.state.bgPos === true){
@@ -95,10 +217,9 @@ class App extends Component {
       
       if(this.state.mode === 0){
           config = (
-            <div>
+            <div id='joinCont'>
             <input type="text" placeholder="Type Username" id='userNameInput'onChange={this.handleUsername}/>
-            <button onClick={this.joinChat}> Join Chat</button>
-
+            <button onClick={this.joinChat} id='joinBtn'>Join Chat</button>
         </div>
       ) 
       
@@ -106,7 +227,7 @@ class App extends Component {
           var allChats = this.state.allChat.map((obj,i)=>{
               return(
               <div key={i}>
-              {obj}
+              <div id='msgs'>{obj}</div>
               </div>
               )
           });
@@ -114,8 +235,8 @@ class App extends Component {
           <div id="chatBox" className={"chatBarHidden" + chatBarShow}>
               
               <div id="controls" >
-              <input type='text' placeholder='type your msg' onChange={this.handleMyMsg} />
               <button onClick={this.sendChat} className="chatBtn">send chat</button>
+              <input  id="msginput" type='text' placeholder='type your msg' onChange={this.handleMyMsg} />
               </div>
               </div>
           )
@@ -128,39 +249,91 @@ class App extends Component {
             </div>
         )
     })  
+	
+	
+//newSHIT
+	
+	var allimages = this.state.allUsers.map((obj,i)=>{
+		  return (
+		  	<img ref={"u"+obj} key={i} height={150} className="allImgs" src={this.state.myImg} />
+		  )
+	  })
+	  
+	  var stickers = this.state.stickers.map((obj,i)=>{
+		  
+		  var mstyle = {left:obj.x, top:obj.y}
+		  return (
+			  
+			  <img className="allImgs" style={mstyle} src={obj.src} key={i} height = {50} />
+		  )
+	  })
+	 var comp = null;
+	  
+	  if(this.state.showDisplay === false){
+		  comp = <Rooms 
+		  changePage={this.changePage}
+		  />
+	  }else{
+		   comp = (
+		  <div>
+		  <div ref="thedisplay" className="div1">
+				{allimages}
+			   	{stickers}
+		</div>
+		<div className="div2">
+			{this.state.myId}
+			<img src={this.state.myImg} height={50} onClick={this.handleImage} />
+			<img src={this.state.myImg2} height={50} onClick={this.handleImage} />
+		</div>
+		  </div>
+			   );
+	  }
+	  
     
     return (
-      <div>
+        <div>
+        
+		<div id="stickerPage" className={"stickerPage" + stickerPageShow}>
+		{comp}
+		</div>
+		
         <div id="chatBox" >
         {config}
         </div>
-       
-        
+        
         <div>
-        
-    
-    <div>
+        <div>
         
-        
-        <div id='nameCont' className="nameCont">
-        Users in the chatroom right now!
+        <div id='nameAll'>
+        <div id='nameCont' className={"nameCont" + nameContShow}>
+        <h1>Chatroom Users</h1>
         <hr />
         <div id='name' className='name'>
-        {allUsers}
+        <h2>{allUsers}</h2>
+        </div>
         </div>
         </div>
         
         <div id='layer1' className={"bg-layer" + bgChange}>
-            
-            
         <img src={layer1}/>
         <img src={layer2}/>
- <div id='chatDisplay' className= {"chatBoxHidden" + chatBoxShow}>
-              {allChats}
-              </div>
         </div>
         
+        <div id='chatDisplay' className= {"chatBoxHidden" + chatBoxShow}>
+        <h3>{allChats}</h3>
+        </div>
         
+		
+		<div id='stickerRooms' className={"stickerRooms" + stickerRoomsShow}>
+		
+		<button onClick={this.showStickerPage}>Sticker Time</button>
+		
+		</div>
+		
+		
+		
+		
+		
         <div>
         <div className='rain'></div>
         <div className='rain'></div>
@@ -192,10 +365,10 @@ class App extends Component {
         <div className='rain'></div>
         <div className='rain'></div>
         <div className='rain'></div>
-        </div>
-
-      </div>
-    
+        
+            </div>
+        
+          </div>
         </div>
       </div>
     );
---
+++ src/comp/Rooms.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import mySocket from "socket.io-client";
+
+class App extends Component {
+	constructor(props){
+		super(props);
+		this.state={
+		}
+	}
+	
+	
+	
+	
+	componentDidMount(){
+		}
+		
+	
+	
+	
+  render() {
+	 
+    return (
+      <div className="App">
+		<button onClick={this.props.changePage.bind(this,"room1")}>Room 1</button>
+		<button onClick={this.props.changePage.bind(this,"room2")}>Room 2</button>
+      </div>
+    );
+  }
+}
+
+export default App;
